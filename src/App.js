3b6031163f94a9445edcfd7d8146df7534bbd4c6
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-11-29 21:15:14 -0500

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Grapes",
+    "Pineapple",
+    "Watermelon",
+    "Mango",
+    "Strawberry",
+    "Papaya",
+    "Peach",
+    "Pear",
+    "Cherry",
+    "Kiwi",
+    "Lemon",
+  ];
+
+  const [filterList, setFilterList] = useState(list);
+  const [word, setWord] = useState("");
+
+  const handleSearch = (e) => {
+
+    if(e.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+
+    const filteredValues = list.filter((item) => item.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1);
+    setFilterList(filteredValues);
+
+    };
+
+  const addWord = (list) => {
+  
+
+    
+  }  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          {/* <form onSubmit={addWord}>
+           Add Word: <input type="text" value={word} onChange={e => setWord(e.target.value)}  /><button  type="submit">Add</button>
+          </form> */}
+        </div>
+      <div>
+        Search: <input name="query" type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+        
+     
     </div>
   );
-}
+};
 
 export default App;
